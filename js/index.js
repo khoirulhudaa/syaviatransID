b87e2fc93a56b0921b7a2fff2230fbee03dca506
valclassdevelop <79193720+valclassdevelop@users.noreply.github.com>
2021-05-13 17:58:49 +0700

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
 document.getElementById('brand').addEventListener('click', () => {
     window.location.reload();
 })
+
+document.getElementById('comment').addEventListener('click', () => {
+    document.getElementById('pageKonsultasi').classList.toggle("pageKonsultasiOpen");
+})
+
+document.getElementById('btnDanger').addEventListener('click', () => {
+    document.getElementById('pageKonsultasi').classList.remove("pageKonsultasiOpen");
+})
